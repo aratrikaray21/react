f85c408531994e7ae37fe1fc354a13d6aef1a7ed
aratrikaray21 <aratrika2109@gmail.com>
2025-12-13 10:58:35 +0530

    Component: Navbar and Card

---
+++ ReactJS/learn/src/components/Card.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+    <div className='card'>
+        <h1 className='card-title'>Hello I am  Aratrika</h1>
+        <p className='card-content'>This is my first React App</p>
+    </div>
+  )
+}
+
+export default Card
