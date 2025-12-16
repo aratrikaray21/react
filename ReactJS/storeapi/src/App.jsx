832e47ce3857143fb81e4f2564c3b3fd10ad7419
aratrikaray21 <aratrika2109@gmail.com>
2025-12-16 10:11:28 +0530

    async,await,fetch-basic

--- ReactJS/storeapi/src/App.jsx
+++ ReactJS/storeapi/src/App.jsx
@@ -1,26 +1,33 @@
 import React from 'react'
+import { useState } from 'react'
 
 const App = () => {
+  const [user, setUser] = useState(null)
 
-  localStorage.setItem('user', 'Aratrika')
-  localStorage.setItem('age', '20')
-  console.log(localStorage.getItem('user'))
-  console.log(localStorage.getItem('age'))
-  localStorage.removeItem('cute_notes_tasks')
-
-  const project={
-    name: 'Cute Notes',
-    version: '1.0.0',
-    description: 'A cute notes app'
+  async function getUser() {
+    try {
+      const response = await fetch('https://jsonplaceholder.typicode.com/users/1')
+      const data = await response.json()
+      console.log(data)
+      setUser(data)
+    }
+    catch (e) {
+      console.log("Error:", e)
+    }
   }
 
-  localStorage.setItem('project', JSON.stringify(project))
-  const str=localStorage.getItem('project')
-  console.log(typeof(str))
-  const savedProject=JSON.parse(localStorage.getItem('project'))
-  console.log(savedProject)
   return (
-    <div>App</div>
+    <>
+      <button className="px-4 py-2 bg-rose-500 text-white rounded-lg hover:bg-rose-600 transition" onClick={getUser}>Click Me</button>
+      {user && (<><p>{user.name}</p>
+        <p>{user.email}</p>
+        <p>{user.phone}</p>
+        <p>{user.website}</p>
+      </>
+      )}
+
+
+    </>
   )
 }
 
