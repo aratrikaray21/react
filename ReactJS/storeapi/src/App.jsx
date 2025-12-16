ba28ec1c2dcaf0c779f111f0b80e9373a9dc1770
aratrikaray21 <aratrika2109@gmail.com>
2025-12-16 10:27:23 +0530

    axios with alt method comments

--- ReactJS/storeapi/src/App.jsx
+++ ReactJS/storeapi/src/App.jsx
@@ -4,6 +4,7 @@ import axios from 'axios'
 
 const App = () => {
   const [user, setUser] = useState(null)
+  //const [user, setUser] = useState([])
 
   const getUser=async()=>{
     const response=await axios.get('https://jsonplaceholder.typicode.com/users')
@@ -12,6 +13,7 @@ const App = () => {
   return (
     <>
       <button className="px-4 py-2 bg-rose-500 text-white rounded-lg hover:bg-rose-600 transition" onClick={getUser}>Click Me</button>
+       {/* {user.map} */}
        {user && user.map((u,idx)=>(
         <div key={idx} className="mt-4 p-4 bg-white rounded-lg shadow">
           <h2 className="text-lg font-semibold">{u.name}</h2>
