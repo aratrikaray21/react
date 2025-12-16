748b48ce9acb01e782499d00933e509c5da54ab6
aratrikaray21 <aratrika2109@gmail.com>
2025-12-16 10:25:42 +0530

    axios api

--- ReactJS/storeapi/src/App.jsx
+++ ReactJS/storeapi/src/App.jsx
@@ -1,34 +1,25 @@
 import React from 'react'
 import { useState } from 'react'
+import axios from 'axios'
 
 const App = () => {
   const [user, setUser] = useState(null)
 
-  async function getUser() {
-    try {
-      const response = await fetch('https://jsonplaceholder.typicode.com/users/1')
-      const data = await response.json()
-      console.log(data)
-      setUser(data)
-    }
-    catch (e) {
-      console.log("Error:", e)
-    }
+  const getUser=async()=>{
+    const response=await axios.get('https://jsonplaceholder.typicode.com/users')
+    setUser(response.data)
   }
-
   return (
     <>
       <button className="px-4 py-2 bg-rose-500 text-white rounded-lg hover:bg-rose-600 transition" onClick={getUser}>Click Me</button>
-       {user && (
-        <div className="mt-4 p-4 bg-white rounded-lg shadow">
-          <h2 className="text-lg font-semibold">{user.name}</h2>
-          <p className="text-sm text-gray-600">{user.email}</p>
-          <p className="text-sm text-gray-600">{user.phone}</p>
-          <p className="text-sm text-gray-600">{user.website}</p>
+       {user && user.map((u,idx)=>(
+        <div key={idx} className="mt-4 p-4 bg-white rounded-lg shadow">
+          <h2 className="text-lg font-semibold">{u.name}</h2>
+          <p className="text-sm text-gray-600">{u.email}</p>
+          <p className="text-sm text-gray-600">{u.phone}</p>
+          <p className="text-sm text-gray-600">{u.website}</p>
         </div>
-      )}
-
-
+      ))}
     </>
   )
 }
